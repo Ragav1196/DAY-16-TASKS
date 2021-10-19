304a55c7aac0b6a616c56dbf7f58925b26b2ba91
Ragav1196 <91084155+Ragav1196@users.noreply.github.com>
2021-10-19 23:57:01 +0530

    Tasks Submitted

---
+++ TASKS/GIVEN IN ZEN CLASS PORTAL/TASK 1/Task 1.js
@@ -0,0 +1,230 @@
+// body *{
+//     font-family: 'Newsreader', serif;
+//     font-weight: 500;
+//     font-size: 15px;
+// }
+
+// /* body{
+//     margin: 0px 150px;
+// } */
+
+// .heading_title{
+//     text-align: center;
+//     font-size: 30px;
+//     font-weight: 800;
+//     font-family: Arial, Helvetica, sans-serif;
+//     margin: 15px 0px;
+// }
+
+// .date_container{
+//     height: 45px;
+//     width: 100%;
+//     font-size: 5px;
+//     background-color: rgb(199, 199, 236);
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+// }
+
+// .year, .t_pages{
+//     margin: 0px;
+// }
+
+
+// /* SIDE CONTAINER */
+
+// .side_container{
+//     display: grid;
+//     grid-template-columns:  1fr;
+//     justify-content: center;
+//     justify-items: center;
+// }
+
+// .sc_sub{
+//     background-color: rgb(186, 223, 248);
+//     height: 230px;
+//     width: 300px;
+//     margin: 10px 10px;
+//     display: grid;
+//     grid-template-columns: 1fr 1fr;
+//     justify-content: center;
+// }
+
+// .sc_side_panel{
+//     height: 225px;
+//     width: 60px;
+//     display: grid;
+//     grid-template-rows: 1fr 1.5fr;
+//     border-style:groove;
+//     background-color: rgb(157, 179, 219);
+// }
+
+// .sc_side_panel_top{
+//     background-color: rgb(106, 151, 235);;
+// }
+
+// .sc_side_panel_img{
+//     height: 32px;
+//     width: 44px;
+//     margin-left: 10px;
+// }
+
+// .sc_side_panel_num{
+//     text-align: center;
+//     font-size: 26px;
+// }
+
+// .sc_main_panel{
+//     height: 230px;
+//     width: 216px;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: flex-end;
+// }
+
+
+// .sc_main_panel_img{
+//     height: 94px;
+//     width: 216px;
+//     object-fit: cover;
+// }
+
+// .sc_main_panel_detail{
+//     padding-top: 20px;
+//     font-weight:500;
+//     font-size: 20px;
+//     text-align: center;
+// }
+
+// .sc_sp_d_text{
+//     writing-mode: vertical-lr;
+//     font-size: 19px;
+// }
+
+// .sc_sp_down{
+//     display: flex;
+//     align-items: center;
+// }
+// /* MAIN CONTAINER */
+
+// .main_container_2 *{
+//     font-weight: 400;
+//     font-size: 15px;
+// }
+
+// .sub_tn_1st_para, .sub_tn_2nd_para, .bottom_news{
+//     margin-top: 100px;
+// }
+
+// .sub_tn_2nd_para_content{
+//     margin-top: 50px;
+// }
+
+// .bold{
+//     font-weight:900;
+//     font-size: 20px;
+// }
+
+// .sub_heading{
+//     font-weight:700;
+//     font-size: 18px;
+// }
+
+// .sub_tn_2nd_para_content_img{
+//     height: 250px;
+//     width: 304px;
+// }
+
+// .bn_content_img{
+//     height: 250px;
+//     width: 304px;
+// }
+
+// @media (min-width: 623px){
+//     .side_container{
+//         grid-template-columns: 1fr 1fr;
+//     }
+// }
+
+// @media (min-width: 942px){
+//     .side_container{
+//         grid-template-columns: 1fr 1fr 1fr;
+//     }
+//     .sub_tn_2nd_para, .sub_tn_1st_para_article{
+//         display: flex;
+//         flex-direction: row;
+//         align-items: stretch;
+//     }
+
+//     .bn_content_img{
+//         margin-left: 320px;
+//     }
+
+//     .bn_content_2{
+//         display: flex;
+//         align-items: center;
+//     }
+
+//     .sub_tn_1st_para_heading{
+//         margin-left: 210px;
+//     }
+
+//     .sub_tn_2nd_para_content_img{
+//         margin-left: 140px;
+//         width: 355px;
+//     }
+//     li{
+//         margin: 20px 100px;
+//     }
+// }
+
+// @media (min-width: 1200px){
+//     .side_container{
+//         grid-template-columns: 1fr;
+//     }
+    
+//     .main_container{
+//         display: flex;
+//     }
+
+//     .sub_tn_1st_para_article{
+//         display: flex;
+//     }
+
+//     .top_news{
+//         display: flex;
+//     }
+
+//     .sub_tn_2{
+//         display: flex;
+//         flex-direction: row;
+//     }
+
+//     .sub_bn{
+//         display: flex;
+//     }
+
+//     .bn_content_img_container{
+//         display: flex;
+//     }
+
+//     .bn_content_img{
+//         height: 390px;
+//         width: 619px;
+//     }
+
+//     .sub_tn_2nd_para_content{
+//         height: 200px;
+//         width: 400px;
+//     }
+
+//     .bn_content_img_container_detail{
+//         margin-top: 150px;
+//     }
+
+//     .sub_tn_2_content{
+//         width: 250px;
+//     }
+    
+// }
